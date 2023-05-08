66286a34f6ddad8fe6817b7bfb92a04ef936fef7
Karan192011114 <113573703+Karan192011114@users.noreply.github.com>
2023-05-08 08:50:36 +0530

    Add files via upload

---
+++ Addtion.R
@@ -0,0 +1,4 @@
+num1=as.integer(readline(prompt = "enter a number"))
+num2=as.integer(readline(prompt = "enter a number"))
+num=num1+num2
+print(num)
---
+++ subraction.R
@@ -0,0 +1,4 @@
+num1=as.integer(readline(prompt = "enter a number1:"))
+num2=as.integer(readline(prompt = "enter a number2:"))
+num=num1-num2
+print(num)
